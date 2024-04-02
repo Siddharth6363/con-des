7f9c11286162edd99b128cb497fd6bfb6258bc8e
Siddharth Chauhan <siddharthc443@gmail.com>
2024-04-02 23:05:21 +0530

    con

---
+++ con1.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+class Wall{
+    public:
+    double length;
+
+    Wall(){       //DEFAULT CONSTRUCTOR
+        length=9.86;
+        cout<<"Creating a wall";
+        cout<<"Length of the wall is: "<<length;
+    }
+    
+
+};
+
+int main()
+{   Wall wall1;
+
+    return 0;
+}
